1cb4744912b7f0daaab7f6a28ad454a20168baa1
emiliopaolini <paolini.emilio5@gmail.com>
2023-11-01 13:14:49 -0500

    Almost finished results for ddos

--- main.py
+++ main.py
@@ -25,16 +25,16 @@ data_tmp = pd.read_csv('data/UNSW/UNSW-NB15_2.csv',header=None,sep=",")
 
 
 # all features
-# X_train_tmp = data_tmp.iloc[:,[4,7,8,9,18,19,43,44]]
-# X_train_tmp.columns = ['IP_proto','s_bytes','d_bytes','s_TTL','swin','dwin','ct_src_ltm','ct_dst_ltm']
+X_train_tmp = data_tmp.iloc[:,[4,7,8,9,18,19,43,44]]
+X_train_tmp.columns = ['IP_proto','s_bytes','d_bytes','s_TTL','swin','dwin','ct_src_ltm','ct_dst_ltm']
 
 # stateless features
-# X_train_tmp = data_tmp.iloc[:,[4,7,8,9,18,19]]
-# X_train_tmp.columns = ['IP_proto','s_bytes','d_bytes','s_TTL','swin','dwin']
+#X_train_tmp = data_tmp.iloc[:,[4,7,8,9,18,19]] 
+#X_train_tmp.columns = ['IP_proto','s_bytes','d_bytes','s_TTL','swin','dwin']
 
 # stateful features
-X_train_tmp = data_tmp.iloc[:,[43,44]]
-X_train_tmp.columns = ['ct_src_ltm','ct_dst_ltm']
+#X_train_tmp = data_tmp.iloc[:,[43,44]]
+#X_train_tmp.columns = ['ct_src_ltm','ct_dst_ltm']
  
 
 
@@ -51,16 +51,16 @@ print(np.unique(y_train_tmp,return_counts=True))
 
 
 # all features
-# X_train = data.iloc[:,[4,7,8,9,18,19,43,44]]
-# X_train.columns = ['IP_proto','s_bytes','d_bytes','s_TTL','swin','dwin','ct_src_ltm','ct_dst_ltm']
+X_train = data.iloc[:,[4,7,8,9,18,19,43,44]]
+X_train.columns = ['IP_proto','s_bytes','d_bytes','s_TTL','swin','dwin','ct_src_ltm','ct_dst_ltm']
 
 # stateless features
-# X_train = data.iloc[:,[4,7,8,9,18,19]]
-# X_train.columns = ['IP_proto','s_bytes','d_bytes','s_TTL','swin','dwin']
+#X_train = data.iloc[:,[4,7,8,9,18,19]]
+#X_train.columns = ['IP_proto','s_bytes','d_bytes','s_TTL','swin','dwin']
  
 # stateful features
-X_train = data.iloc[:,[43,44]]
-X_train.columns = ['ct_src_ltm','ct_dst_ltm']
+#X_train = data.iloc[:,[43,44]]
+#X_train.columns = ['ct_src_ltm','ct_dst_ltm']
 
 
 
@@ -75,13 +75,13 @@ print(np.unique(y_train,return_counts=True))
 
 
 X_train = X_train.dropna()
-#X_train['IP_proto'] = X_train['IP_proto'].astype('category').cat.codes
+X_train['IP_proto'] = X_train['IP_proto'].astype('category').cat.codes
 
-'''
+
 scaler = MinMaxScaler()
 
 X_train[X_train.columns] = scaler.fit_transform(X_train[X_train.columns])
-'''
+
 
 X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, test_size=0.25,stratify=y_train,shuffle=True)
 
@@ -126,42 +126,36 @@ def f1_m(y_true, y_pred):
     return 2*((precision*recall)/(precision+recall+K.epsilon()))
   
 
-#input_1 = X_train['IP_proto']
-#input_1 = X_train['swin']
-#input_2 = X_train['quantized_s_TTL'] #11
-#input_2 = X_train['s_TTL']
+input_1 = X_train['swin']
+input_2 = X_train['s_TTL']
 
 
-#input_3 = X_train['dwin']
-#input_4 = X_train['quantized_proto'] #11 
-#input_4 = X_train['IP_proto']
+input_3 = X_train['dwin']
+input_4 = X_train['IP_proto']
 
 input_5 = X_train['ct_src_ltm']
-#input_6 = X_train['quantized_dbytes'] #13
+input_6 = X_train['d_bytes'] #13
 
 input_7 = X_train['ct_dst_ltm']
-#input_8 = X_train['quantized_sbytes'] #13
+input_8 = X_train['s_bytes'] #13
 
 
 
+input_1_val = X_test['swin']
+input_2_val = X_test['s_TTL']
 
-#input_1_val = X_test['swin']
-#input_2_val = X_test['quantized_s_TTL']
-#input_2_val = X_test['s_TTL']
-
-#input_3_val = X_test['dwin']
-#input_4_val = X_test['quantized_proto']
-#input_4_val = X_test['IP_proto']
+input_3_val = X_test['dwin']
+input_4_val = X_test['IP_proto']
 
 input_5_val = X_test['ct_src_ltm']
-#input_6_val = X_test['quantized_dbytes']
-#input_7_val = X_test['s_TTL']
+input_6_val = X_test['d_bytes']
+
 input_7_val = X_test['ct_dst_ltm']
-#input_8_val = X_test['quantized_sbytes']
+input_8_val = X_test['s_bytes']
 
 
 
-opt = keras.optimizers.Adam(0.01)
+opt = keras.optimizers.Adam(0.001)
 model = nn.nn()
 
 print(model.summary())
@@ -175,20 +169,22 @@ checkpoint = ModelCheckpoint(filepath=checkpoint_filepath, monitor='val_f1_m',
                             verbose=1, save_best_only=True,save_weights_only=True, mode='max')
 
 # all features
-#history = model7.fit([input_1,input_2,input_3,input_4,input_5,input_6,input_7,input_8], y_train,
-#                    validation_data = ([input_1_val,input_2_val,input_3_val,input_4_val,input_5_val,input_6_val,input_7_val,input_8_val],y_test)
-#                    ,batch_size=256,epochs=10,shuffle=True,callbacks=[checkpoint])
+history = model.fit([input_1,input_2,input_3,input_4,input_5,input_6,input_7,input_8], y_train,
+                    validation_data = ([input_1_val,input_2_val,input_3_val,input_4_val,input_5_val,input_6_val,input_7_val,input_8_val],y_test)
+                    ,batch_size=256,epochs=10,shuffle=True,callbacks=[checkpoint])
 # stateless
-# history = model7.fit([input_1,input_2,input_3,input_4,input_6,input_8], y_train,
+#history = model.fit([input_1,input_2,input_3,input_4,input_6,input_8], y_train,
 #                    validation_data = ([input_1_val,input_2_val,input_3_val,input_4_val,input_6_val,input_8_val],y_test)
 #                    ,batch_size=256,epochs=10,shuffle=True,callbacks=[checkpoint])
 # stateful
-history = model.fit([input_5,input_7], y_train,
-                    validation_data = ([input_5_val,input_7_val],y_test)
-                    ,batch_size=256,epochs=20,shuffle=True,callbacks=[checkpoint])
+#history = model.fit([input_5,input_7], y_train,
+#                    validation_data = ([input_5_val,input_7_val],y_test)
+#                    ,batch_size=256,epochs=20,shuffle=True,callbacks=[checkpoint])
 
-#model7.load_weights(checkpoint_filepath)
 model.load_weights(checkpoint_filepath)
 
-model.save('models/iot_stateful_4_bit.h5')
-print(model.evaluate([input_5_val,input_7_val],y_test,batch_size=256))
+model.save('models/unsw_stateless_4_bit.h5')
+
+#print(model.evaluate([input_5_val,input_7_val],y_test,batch_size=256))
+#print(model.evaluate([input_1_val,input_2_val,input_3_val,input_4_val,input_6_val,input_8_val],y_test,batch_size=256))
+print(model.evaluate([input_1_val,input_2_val,input_3_val,input_4_val,input_5_val,input_6_val,input_7_val,input_8_val],y_test,batch_size=256))
--- nn.py
+++ nn.py
@@ -1,15 +1,20 @@
 import larq as lq
 from tensorflow.keras.layers import Input, Dense, Concatenate, Add, BatchNormalization
 from tensorflow.keras import Model
+from keras.utils.vis_utils import plot_model
 
 
 def nn():
+    
     # FIRST MODELS
-    '''
+    bitwidth = 6
+    
     # MODEL 1
     model1_in_1 = Input(shape=(1,),name='input1')
+    model1_in_1_quant = lq.quantizers.DoReFa(k_bit=bitwidth, mode="activations",name='quant_input_11')(model1_in_1)
     model1_in_2 = Input(shape=(1,),name='input2')
-    concatenated_1 = Concatenate()([model1_in_1, model1_in_2])
+    model1_in_2_quant = lq.quantizers.DoReFa(k_bit=bitwidth, mode="activations",name='quant_input_12')(model1_in_2)
+    concatenated_1 = Concatenate()([model1_in_1_quant, model1_in_2_quant])
 
 
     #model1_out_1 = lq.layers.QuantDense(256, activation='relu', name='layer_1_1', **kwargs)(concatenated_1)
@@ -17,17 +22,19 @@ def nn():
     bn = BatchNormalization(scale=False,center=False)(model1_out_1)
     model1_out_11 = Dense(32, activation='relu', name='layer_1_11')(bn)
     bn2 = BatchNormalization(scale=False,center=False)(model1_out_11)
-    model1_out_2 = Dense(2, activation='relu', name='layer_1_2')(bn2)
+    model1_out_2 = Dense(1, activation='relu', name='layer_1_2')(bn2)
 
     model1 = Model([model1_in_1,model1_in_2], model1_out_2)
 
     plot_model(model1, to_file='model_plot.png', show_shapes=True, show_layer_names=True)
 
-
+    
     # MODEL 2
     model2_in_1 = Input(shape=(1,),name='input3')
+    model2_in_1_quant = lq.quantizers.DoReFa(k_bit=bitwidth, mode="activations",name='quant_input_21')(model2_in_1)
     model2_in_2 = Input(shape=(1,),name='input4')
-    concatenated_2 = Concatenate()([model2_in_1, model2_in_2])
+    model2_in_2_quant = lq.quantizers.DoReFa(k_bit=bitwidth, mode="activations",name='quant_input_22')(model2_in_2)
+    concatenated_2 = Concatenate()([model2_in_1_quant, model2_in_2_quant])
 
 
     #model2_out_1 = lq.layers.QuantDense(256, activation='relu', name='layer_2_1', **kwargs)(concatenated_2)
@@ -35,7 +42,7 @@ def nn():
     bn = BatchNormalization(scale=False,center=False)(model2_out_1)
     model2_out_11 = Dense(32, activation='relu', name='layer_2_11')(bn)
     bn2 = BatchNormalization(scale=False,center=False)(model2_out_11)
-    model2_out_2 = Dense(2, activation='relu', name='layer_2_2')(bn2)
+    model2_out_2 = Dense(1, activation='relu', name='layer_2_2')(bn2)
 
     model2 = Model([model2_in_1,model2_in_2], model2_out_2)
 
@@ -44,71 +51,82 @@ def nn():
 
     # MODEL 3
     model3_in_1 = Input(shape=(1,),name='input5')
+    model3_in_1_quant = lq.quantizers.DoReFa(k_bit=bitwidth, mode="activations",name='quant_input_31')(model3_in_1)
     model3_in_2 = Input(shape=(1,),name='input6')
-    concatenated_3 = Concatenate()([model3_in_1, model3_in_2])
+    model3_in_2_quant = lq.quantizers.DoReFa(k_bit=bitwidth, mode="activations",name='quant_input_32')(model3_in_2)
+    concatenated_3 = Concatenate()([model3_in_1_quant, model3_in_2_quant])
 
     #model3_out_1 = lq.layers.QuantDense(256, activation='relu', name='layer_3_1', **kwargs)(concatenated_3)
     model3_out_1 = Dense(64, activation='relu', name='layer_3_1')(concatenated_3)
     bn = BatchNormalization(scale=False,center=False)(model3_out_1)
     model3_out_11 = Dense(32, activation='relu', name='layer_3_11')(bn)
     bn2 = BatchNormalization(scale=False,center=False)(model3_out_11)
-    model3_out_2 = Dense(2, activation='relu', name='layer_3_2')(bn2)
+    model3_out_2 = Dense(1, activation='relu', name='layer_3_2')(bn2)
 
     model3 = Model([model3_in_1,model3_in_2], model3_out_2)
 
     plot_model(model3, to_file='model_plot_3.png', show_shapes=True, show_layer_names=True)
 
-
+    
+    
     # MODEL 4
     model4_in_1 = Input(shape=(1,),name='input7')
+    model4_in_1_quant = lq.quantizers.DoReFa(k_bit=bitwidth, mode="activations",name='quant_input_41')(model4_in_1)
     model4_in_2 = Input(shape=(1,),name='input8')
-    concatenated_4 = Concatenate()([model4_in_1, model4_in_2])
+    model4_in_2_quant = lq.quantizers.DoReFa(k_bit=bitwidth, mode="activations",name='quant_input_42')(model4_in_2)
+    concatenated_4 = Concatenate()([model4_in_1_quant, model4_in_2_quant])
 
     #model4_out_1 = lq.layers.QuantDense(256, activation='relu', name='layer_4_1', **kwargs)(concatenated_4)
     model4_out_1 = Dense(64, activation='relu', name='layer_4_1')(concatenated_4)
     bn = BatchNormalization(scale=False,center=False)(model4_out_1)
     model4_out_11 = Dense(32, activation='relu', name='layer_4_11')(bn)
     bn2 = BatchNormalization(scale=False,center=False)(model4_out_11)
-    model4_out_2 = Dense(2, activation='relu', name='layer_4_2')(bn2)
+    model4_out_2 = Dense(1, activation='relu', name='layer_4_2')(bn2)
 
     model4 = Model([model4_in_1,model4_in_2], model4_out_2)
 
     plot_model(model4, to_file='model_plot_4.png', show_shapes=True, show_layer_names=True)
+    
 
-
+    
+    
     # SECOND LAYER MODELS
 
-    bitwidth = 4
-    kwargs = dict(input_quantizer=lq.quantizers.DoReFa(k_bit=bitwidth, mode="activations"),
-                use_bias=False
-    )
+
 
     # MODEL 5
-    concatenated_5 = Concatenate()([model1.output, model2.output])
 
-    model5_out_1  = lq.layers.QuantDense(64, activation='relu', name='layer_5_1', **kwargs)(concatenated_5)
+    concatenated_5_quant_0 = lq.quantizers.DoReFa(k_bit=bitwidth, mode="activations",name='quant_input_5_1')(model1.output)
+    concatenated_5_quant_1 = lq.quantizers.DoReFa(k_bit=bitwidth, mode="activations",name='quant_input_5_2')(model2.output)
+    concatenated_5 = Concatenate()([concatenated_5_quant_0, concatenated_5_quant_1])
+    
+
+    model5_out_1  = Dense(64, activation='relu', name='layer_5_1')(concatenated_5)
     bn = BatchNormalization(scale=False,center=False)(model5_out_1)
     model5_out_11 = Dense(32, activation='relu', name='layer_5_11')(bn)
     bn2 = BatchNormalization(scale=False,center=False)(model5_out_11)
-    model5_out_2 = Dense(2, activation='relu', name='layer_5_2')(bn2)
+    model5_out_2 = Dense(1, activation='relu', name='layer_5_2')(bn2)
 
 
     model5 = Model([model1.input, model2.input], model5_out_2)
 
 
     plot_model(model5, to_file='model_plot_5.png', show_shapes=True, show_layer_names=True)
-    '''
+    
 
 
-    '''
+    
     # MODEL 6
-    concatenated_6 = Concatenate()([model3.output, model4.output])
+    concatenated_6_quant_0 = lq.quantizers.DoReFa(k_bit=bitwidth, mode="activations",name='quant_input_6_1')(model3.output)
+    concatenated_6_quant_1 = lq.quantizers.DoReFa(k_bit=bitwidth, mode="activations",name='quant_input_6_2')(model4.output)
 
-    model6_out_1  = lq.layers.QuantDense(64, activation='relu', name='layer_6_1', **kwargs)(concatenated_6)
+    concatenated_6 = Concatenate()([concatenated_6_quant_0, concatenated_6_quant_1])
+
+    model6_out_1  = Dense(64, activation='relu', name='layer_6_1')(concatenated_6)
     bn = BatchNormalization(scale=False,center=False)(model6_out_1)
     model6_out_11 = Dense(32, activation='relu', name='layer_6_11')(bn)
     bn2 = BatchNormalization(scale=False,center=False)(model6_out_11)
-    model6_out_2 = Dense(2, activation='relu', name='layer_6_2')(bn2)
+    model6_out_2 = Dense(1, activation='relu', name='layer_6_2')(bn2)
 
 
     model6 = Model([model3.input, model4.input], model6_out_2)
@@ -120,38 +138,55 @@ def nn():
     # THIRD LAYER MODELS
 
     # MODEL 7
-    concatenated_7 = Concatenate()([model5.output, model6.output])
+    concatenated_7_quant_0 = lq.quantizers.DoReFa(k_bit=bitwidth, mode="activations",name='quant_input_7_1')(model5.output)
+    concatenated_7_quant_1 = lq.quantizers.DoReFa(k_bit=bitwidth, mode="activations",name='quant_input_7_2')(model6.output)
+    concatenated_7 = Concatenate()([concatenated_7_quant_0, concatenated_7_quant_1])
 
-    model7_out_1 =  lq.layers.QuantDense(128, activation='relu', name='output_layer_7_1', **kwargs)(concatenated_7)
+    model7_out_1 =  Dense(128, activation='relu', name='output_layer_7_1')(concatenated_7)
     bn = BatchNormalization(scale=False,center=False)(model7_out_1)
 
     model7_out_11 = Dense(64, activation='relu', name='output_layer_7_11')(bn)
     bn1 = BatchNormalization(scale=False,center=False)(model7_out_11)
 
     model7_out_2 = Dense(1, activation='sigmoid', name='output_layer_7_2')(bn1)
+    model7_out_2_quant = lq.quantizers.DoReFa(k_bit=bitwidth, mode="activations",name='quant_output_1')(model7_out_2)
 
-    model7 = Model([model5.input, model6.input], model7_out_2)
-
+    model7 = Model([model5.input, model6.input], model7_out_2_quant)
+    
     plot_model(model7, to_file='model_plot_7.png', show_shapes=True, show_layer_names=True)
+    return model7
+    '''
 
 
+
+    
     # for stateless features
 
-    concatenated_7 = Concatenate()([model3.output, model5.output])
 
-    model7_out_1 =  lq.layers.QuantDense(128, activation='relu', name='output_layer_7_1', **kwargs)(concatenated_7)
+    concatenated_7_quant_0 = lq.quantizers.DoReFa(k_bit=bitwidth, mode="activations",name='quant_input_7_1')(model3.output)
+    concatenated_7_quant_1 = lq.quantizers.DoReFa(k_bit=bitwidth, mode="activations",name='quant_input_7_2')(model5.output)
+
+    concatenated_7 = Concatenate()([concatenated_7_quant_0,concatenated_7_quant_1])
+
+    model7_out_1 =  Dense(128, activation='relu', name='output_layer_7_1',)(concatenated_7)
     bn = BatchNormalization(scale=False,center=False)(model7_out_1)
 
     model7_out_11 = Dense(64, activation='relu', name='output_layer_7_11')(bn)
     bn1 = BatchNormalization(scale=False,center=False)(model7_out_11)
 
     model7_out_2 = Dense(1, activation='sigmoid', name='output_layer_7_2')(bn1)
+    model7_out_2_quant = lq.quantizers.DoReFa(k_bit=bitwidth, mode="activations",name='quant_output_1')(model7_out_2)
 
-    model7 = Model([model3.input, model5.input], model7_out_2)
+    model7 = Model([model3.input, model5.input], model7_out_2_quant)
 
     plot_model(model7, to_file='model_plot_7.png', show_shapes=True, show_layer_names=True)
 
-    '''
+    return model7
+
+
+
+
+    
     # For stateful
 
 
@@ -159,20 +194,21 @@ def nn():
 
 
     model1_in_1 = Input(shape=(1,),name='input1')
-    model1_in_1 = lq.quantizers.DoReFa(k_bit=bitwidth, mode="activations",name='quant_input_1')(model1_in_1)
+    model1_in_1_quant = lq.quantizers.DoReFa(k_bit=bitwidth, mode="activations",name='quant_input_1')(model1_in_1)
 
     model1_in_2 = Input(shape=(1,),name='input2')
-    model1_in_2 = lq.quantizers.DoReFa(k_bit=bitwidth, mode="activations",name='quant_input_1')(model1_in_2)
-    concatenated_1 = Concatenate()([model1_in_1, model1_in_2])
+    model1_in_2_quant = lq.quantizers.DoReFa(k_bit=bitwidth, mode="activations",name='quant_input_2')(model1_in_2)
+    concatenated_1 = Concatenate()([model1_in_1_quant, model1_in_2_quant])
 
-    model1_out_1 = Dense(32, activation='relu', name='layer_1_1')(concatenated_1)
+    model1_out_1 = Dense(64, activation='relu', name='layer_1_1')(concatenated_1)
     bn1 = BatchNormalization()(model1_out_1)
-    model1_out_11 = Dense(16, activation='relu', name='output_layer_1_11')(bn1)
+    model1_out_11 = Dense(32, activation='relu', name='output_layer_1_11')(bn1)
     bn2 = BatchNormalization()(model1_out_11)
     model1_out_2 = Dense(1, activation='sigmoid', name='output_layer_1_2')(bn2)
-    #model1_out_2 = lq.quantizers.DoReFa(k_bit=bitwidth, mode="activations",name='quant_input_1')(model1_out_2)
+    model1_out_2_quant = lq.quantizers.DoReFa(k_bit=bitwidth, mode="activations",name='quant_output_1')(model1_out_2)
     #model1_out_11 = Dense(32, activation='relu', name='layer_1_11')(model1_out_1)
-    #model1_out_2 = lq.layers.QuantDense(1, activation='relu', name='layer_1_2', **kwargs)(model1_out_1)
-
+    #model1_out_2_quant = lq.layers.QuantDense(1, activation='relu', name='layer_1_2', **kwargs)(model1_out_1)
+    model = Model([model1_in_1,model1_in_2], model1_out_2_quant)
+    plot_model(model, to_file='model.png', show_shapes=True, show_layer_names=True)
     return Model([model1_in_1,model1_in_2], model1_out_2)
-    
+    '''
