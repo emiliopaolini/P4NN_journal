f47d6552922f66ff8fc5591a750e171c511262c1
emiliopaolini <paolini.emilio5@gmail.com>
2023-12-11 21:00:52 -0600

    added traffic_class.py experiment

--- nn.py
+++ nn.py
@@ -118,7 +118,7 @@ def nn(bitwidth, LUT,class_number):
 
         return model7
 
-    if LUT == 9:
+    if LUT == 8:
 
 
         # MODEL 1
@@ -130,11 +130,11 @@ def nn(bitwidth, LUT,class_number):
 
 
         #model1_out_1 = lq.layers.QuantDense(256, activation='relu', name='layer_1_1', **kwargs)(concatenated_1)
-        model1_out_1 = Dense(256, activation='relu', name='layer_1_1')(concatenated_1)
+        model1_out_1 = Dense(64, activation='relu', name='layer_1_1')(concatenated_1)
         bn_1_1 = BatchNormalization()(model1_out_1)
-        model1_out_2 = Dense(128, activation='relu', name='layer_1_12')(bn_1_1)
+        model1_out_2 = Dense(32, activation='relu', name='layer_1_12')(bn_1_1)
         bn_1_12 = BatchNormalization()(model1_out_2)
-        model1_out_11 = Dense(64, activation='relu', name='layer_1_11')(bn_1_12)
+        model1_out_11 = Dense(16, activation='relu', name='layer_1_11')(bn_1_12)
         bn_1_2 = BatchNormalization()(model1_out_11)
         model1_out_2 = Dense(1, activation='relu', name='layer_1_2')(bn_1_2)
 
@@ -150,11 +150,11 @@ def nn(bitwidth, LUT,class_number):
         concatenated_2 = Concatenate()([model2_in_1_quant, model2_in_2_quant])
 
         #model2_out_1 = lq.layers.QuantDense(256, activation='relu', name='layer_2_1', **kwargs)(concatenated_2)
-        model2_out_1 = Dense(256, activation='relu', name='layer_2_1')(concatenated_2)
+        model2_out_1 = Dense(64, activation='relu', name='layer_2_1')(concatenated_2)
         bn_2_1 = BatchNormalization()(model2_out_1)
-        model2_out_12 = Dense(128, activation='relu', name='layer_2_21')(bn_2_1)
+        model2_out_12 = Dense(32, activation='relu', name='layer_2_21')(bn_2_1)
         bn_2_12 = BatchNormalization()(model2_out_12)
-        model2_out_11 = Dense(64, activation='relu', name='layer_2_11')(bn_2_12)
+        model2_out_11 = Dense(16, activation='relu', name='layer_2_11')(bn_2_12)
         bn_2_2 = BatchNormalization()(model2_out_11)
         model2_out_2 = Dense(1, activation='relu', name='layer_2_2')(bn_2_2)
 
@@ -170,14 +170,14 @@ def nn(bitwidth, LUT,class_number):
         concatenated_3 = Concatenate()([model3_in_1_quant, model3_in_2_quant])
 
         #model3_out_1 = lq.layers.QuantDense(256, activation='relu', name='layer_3_1', **kwargs)(concatenated_3)
-        model3_out_1 = Dense(256, activation='relu', name='layer_3_1')(concatenated_3)
+        model3_out_1 = Dense(64, activation='relu', name='layer_3_1')(concatenated_3)
         bn_3_1 = BatchNormalization()(model3_out_1)
         
-        model3_out_12 = Dense(128, activation='relu', name='layer_3_12')(bn_3_1)
+        model3_out_12 = Dense(32, activation='relu', name='layer_3_12')(bn_3_1)
         bn_3_12 = BatchNormalization()(model3_out_12)
 
 
-        model3_out_11 = Dense(64, activation='relu', name='layer_3_11')(bn_3_12)
+        model3_out_11 = Dense(16, activation='relu', name='layer_3_11')(bn_3_12)
         bn_3_2 = BatchNormalization()(model3_out_11)
         
         model3_out_2 = Dense(1, activation='relu', name='layer_3_2')(bn_3_2)
@@ -194,13 +194,13 @@ def nn(bitwidth, LUT,class_number):
         concatenated_4 = Concatenate()([model4_in_1_quant, model4_in_2_quant])
 
         #model4_out_1 = lq.layers.QuantDense(256, activation='relu', name='layer_4_1', **kwargs)(concatenated_4)
-        model4_out_1 = Dense(256, activation='relu', name='layer_4_1')(concatenated_4)
+        model4_out_1 = Dense(64, activation='relu', name='layer_4_1')(concatenated_4)
         bn_4_1 = BatchNormalization()(model4_out_1)
 
-        model4_out_12 = Dense(128, activation='relu', name='layer_4_12')(bn_4_1)
+        model4_out_12 = Dense(32, activation='relu', name='layer_4_12')(bn_4_1)
         bn_4_12 = BatchNormalization()(model4_out_12)
 
-        model4_out_11 = Dense(64, activation='relu', name='layer_4_11')(bn_4_12)
+        model4_out_11 = Dense(16, activation='relu', name='layer_4_11')(bn_4_12)
         bn_4_2 = BatchNormalization()(model4_out_11)
         model4_out_2 = Dense(1, activation='relu', name='layer_4_2')(bn_4_2)
 
@@ -215,14 +215,14 @@ def nn(bitwidth, LUT,class_number):
         new_concatenated_4 = Concatenate()([new_model4_in_1_quant, new_model4_in_2_quant])
 
         #model4_out_1 = lq.layers.QuantDense(256, activation='relu', name='layer_4_1', **kwargs)(concatenated_4)
-        new_model4_out_1 = Dense(256, activation='relu', name='new_layer_5_1')(new_concatenated_4)
+        new_model4_out_1 = Dense(64, activation='relu', name='new_layer_5_1')(new_concatenated_4)
         new_bn_4_1 = BatchNormalization()(new_model4_out_1)
 
-        model5_out_12 = Dense(128, activation='relu', name='layer_5_12')(new_bn_4_1)
+        model5_out_12 = Dense(32, activation='relu', name='layer_5_12')(new_bn_4_1)
         bn_5_12 = BatchNormalization()(model5_out_12)
 
 
-        new_model4_out_11 = Dense(64, activation='relu', name='new_layer_4_11')(bn_5_12)
+        new_model4_out_11 = Dense(16, activation='relu', name='new_layer_4_11')(bn_5_12)
         new_bn_4_2 = BatchNormalization()(new_model4_out_11)
         new_model4_out_2 = Dense(1, activation='relu', name='new_layer_4_2')(new_bn_4_2)
 
@@ -237,16 +237,16 @@ def nn(bitwidth, LUT,class_number):
         concatenated_5_quant_1 = lq.quantizers.DoReFa(k_bit=bitwidth, mode="activations",name='quant_input_5_2')(model2.output)
         concatenated_5 = Concatenate()([concatenated_5_quant_0, concatenated_5_quant_1])
 
-        model5_out_1  = Dense(256, activation='relu', name='layer_5_1')(concatenated_5)
+        model5_out_1  = Dense(64, activation='relu', name='layer_5_1')(concatenated_5)
         bn_5_1 = BatchNormalization()(model5_out_1)
 
 
-        model6_out_12 = Dense(128, activation='relu', name='layer_6_12')(bn_5_1)
+        model6_out_12 = Dense(32, activation='relu', name='layer_6_12')(bn_5_1)
         bn_6_12 = BatchNormalization()(model6_out_12)
 
 
 
-        model5_out_11 = Dense(64, activation='relu', name='layer_5_11')(bn_6_12)
+        model5_out_11 = Dense(16, activation='relu', name='layer_5_11')(bn_6_12)
         bn_5_2 = BatchNormalization()(model5_out_11)
         model5_out_2 = Dense(1, activation='relu', name='layer_5_2')(bn_5_2)
 
@@ -261,13 +261,13 @@ def nn(bitwidth, LUT,class_number):
         concatenated_6 = Concatenate()([concatenated_6_quant_0, concatenated_6_quant_1])
 
 
-        model6_out_1  = Dense(256, activation='relu', name='layer_6_1')(concatenated_6)
+        model6_out_1  = Dense(64, activation='relu', name='layer_6_1')(concatenated_6)
         bn_6_1 = BatchNormalization()(model6_out_1)
 
-        model7_out_12 = Dense(128, activation='relu', name='layer_7_12')(bn_6_1)
+        model7_out_12 = Dense(32, activation='relu', name='layer_7_12')(bn_6_1)
         bn_7_12 = BatchNormalization()(model7_out_12)
 
-        model6_out_11 = Dense(64, activation='relu', name='layer_6_11')(bn_7_12)
+        model6_out_11 = Dense(16, activation='relu', name='layer_6_11')(bn_7_12)
         bn_6_2 = BatchNormalization()(model6_out_11)
         model6_out_2 = Dense(1, activation='relu', name='layer_6_2')(bn_6_2)
 
@@ -286,15 +286,15 @@ def nn(bitwidth, LUT,class_number):
         concatenated_7_quant_1 = lq.quantizers.DoReFa(k_bit=bitwidth, mode="activations",name='quant_input_7_2')(model6.output)
         concatenated_7 = Concatenate()([concatenated_7_quant_0, concatenated_7_quant_1])
 
-        model7_out_3 = Dense(256, activation='relu', name='output_layer_7_3')(concatenated_7)
+        model7_out_3 = Dense(64, activation='relu', name='output_layer_7_3')(concatenated_7)
         bn_7_1 = BatchNormalization()(model7_out_3)
 
         
-        model8_out_12 = Dense(128, activation='relu', name='layer_8_12')(bn_7_1)
+        model8_out_12 = Dense(32, activation='relu', name='layer_8_12')(bn_7_1)
         bn_8_12 = BatchNormalization()(model8_out_12)
 
 
-        model7_out_4 = Dense(64, activation='relu', name='output_layer_7_4')(bn_8_12)
+        model7_out_4 = Dense(16, activation='relu', name='output_layer_7_4')(bn_8_12)
         bn_7_2 = BatchNormalization()(model7_out_4)
 
         model7_out_5 = Dense(1, activation='relu', name='output_layer_7_5')(bn_7_2)
@@ -302,12 +302,12 @@ def nn(bitwidth, LUT,class_number):
         model7 = Model([model5.input, model6.input], model7_out_5)
 
 
-
+        '''
         new_concatenated_7_quant_0 = lq.quantizers.DoReFa(k_bit=bitwidth, mode="activations",name='new_quant_input_7_1')(model7.output)
         new_concatenated_7_quant_1 = lq.quantizers.DoReFa(k_bit=bitwidth, mode="activations",name='new_quant_input_7_2')(new_model4.output)
         new_concatenated_7 = Concatenate()([new_concatenated_7_quant_0, new_concatenated_7_quant_1])
 
-        new_model7_out_3 = Dense(256, activation='relu', name='new_output_layer_7_3')(new_concatenated_7)
+        new_model7_out_3 = Dense(64, activation='relu', name='new_output_layer_7_3')(new_concatenated_7)
         new_bn_7_1 = BatchNormalization()(new_model7_out_3)
 
         model9_out_12 = Dense(128, activation='relu', name='layer_9_12')(new_bn_7_1)
@@ -320,7 +320,7 @@ def nn(bitwidth, LUT,class_number):
         new_model7_out_2_quant = lq.quantizers.DoReFa(k_bit=bitwidth, mode="activations",name='new_quant_output_1')(new_model7_out_5)
 
         new_model7 = Model([model7.input, new_model4.input], new_model7_out_2_quant)
-
+        '''
         
-        plot_model(new_model7, to_file='model.png', show_shapes=True, show_layer_names=True)
-        return new_model7
+        plot_model(model7, to_file='model.png', show_shapes=True, show_layer_names=True)
+        return model7
---
+++ traffic_class.py
@@ -0,0 +1,162 @@
+import pandas as pd
+import numpy as np
+import new_nn as nn
+
+import sklearn.feature_selection as fs
+from sklearn.utils import class_weight
+from tensorflow import keras
+from keras import backend as K
+from keras.callbacks import ModelCheckpoint
+from sklearn.metrics import f1_score as f1
+from sklearn.model_selection import train_test_split
+from imblearn.over_sampling import RandomOverSampler
+from sklearn.preprocessing import MinMaxScaler
+from imblearn.under_sampling import RandomUnderSampler
+from sklearn.preprocessing import LabelEncoder
+
+def recall_m(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    recall = true_positives / (possible_positives + K.epsilon())
+    return recall
+
+def precision_m(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    precision = true_positives / (predicted_positives + K.epsilon())
+    return precision
+
+def f1_m(y_true, y_pred):
+    precision = precision_m(y_true, y_pred)
+    recall = recall_m(y_true, y_pred)
+    return 2*((precision*recall)/(precision+recall+K.epsilon()))
+
+# data from: https://www.kaggle.com/datasets/mrwellsdavid/unsw-nb15?select=UNSW-NB15_LIST_EVENTS.csv
+
+data = pd.read_csv("./data/Unicauca-dataset-April-June-2019-Network-flows.csv",header=0,sep=',')
+
+data = data.drop(['flow_key'],axis=1)
+
+
+
+data.replace([np.inf, -np.inf], np.nan, inplace=True)
+data.dropna(inplace=True)
+data.drop_duplicates(inplace = True)
+
+
+requiredProtocolName = ['TLS','DNS','HTTP']
+
+
+data = data.loc[data['application_protocol'].isin(requiredProtocolName)]
+
+print(data["application_protocol"].value_counts())
+
+
+data.drop(['category','web_service','flowEndReason','src_ip_numeric','src_ip','dst_ip','dst_port','src_port'],axis=1,inplace=True)
+
+data = data[data.columns.drop(list(data.filter(regex='std_dev')))]
+data = data[data.columns.drop(list(data.filter(regex='avg')))]
+
+COLUMNS = data.columns
+
+encoder = LabelEncoder().fit(data['application_protocol'])
+data['application_protocol'] = encoder.transform(data['application_protocol'])
+print(data['application_protocol'])
+
+
+x_dataset = data.drop(['application_protocol'], axis = 1)
+X = x_dataset.to_numpy()
+y_dataset = data['application_protocol']
+y = y_dataset.to_numpy()
+
+print("X SHAPE: ",X.shape)
+print("Y SHAPE: ",y.shape)
+
+FEATURE_NUMBERS = 8
+BITWIDTH = 4
+CLASS_NUMBER = 3
+
+selector = fs.SelectKBest(fs.f_classif, k=FEATURE_NUMBERS)
+X = selector.fit_transform(X, y)
+
+best_columns = selector.get_support(indices=True)
+print("Best features: ",COLUMNS[best_columns])
+
+# Dataset normalization
+scaler = MinMaxScaler()
+X = scaler.fit_transform(X)
+
+
+print("Y distribution: ",np.unique(y,return_counts=True))
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33,stratify=y)
+
+print("Y train: ",np.unique(y_train,return_counts=True))
+print("Y test: ",np.unique(y_test,return_counts=True))
+
+if FEATURE_NUMBERS==8:
+    # instantiate full model
+    print("Instantiatin model with 7 LUTs")
+    model = nn.nn(bitwidth=BITWIDTH,LUT=7,class_number=CLASS_NUMBER)
+if FEATURE_NUMBERS==6:
+    # instantiate middle model
+    print("Instantiatin model with 5 LUTs")
+    model = nn.nn(bitwidth=BITWIDTH,LUT=5,class_number=CLASS_NUMBER)
+if FEATURE_NUMBERS==2:
+    # instantiate small model
+    print("Instantiatin model with 1 LUT")
+    model = nn.nn(bitwidth=BITWIDTH,LUT=1,class_number=CLASS_NUMBER)
+ 
+class_weights = class_weight.compute_class_weight(
+                                        class_weight = "balanced",
+                                        classes = np.unique(y_train),
+                                        y = np.array(y_train)
+                                    )
+
+dic_weights = dict(zip(np.unique(y_train), class_weights))
+print("DICT",dic_weights)
+
+opt = keras.optimizers.Adam()
+model.compile(loss='sparse_categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
+
+checkpoint_filepath = '/tmp/model'+str(BITWIDTH)+'all'
+
+checkpoint = ModelCheckpoint(filepath=checkpoint_filepath, monitor='val_accuracy',
+                            verbose=1, save_best_only=True,save_weights_only=True, mode='max')
+
+print(y_train.shape)
+print(X_train.shape)
+
+if FEATURE_NUMBERS==8:
+    # instantiate full model
+    history = model.fit([X_train[:,0],X_train[:,1],X_train[:,2],X_train[:,3],X_train[:,4],X_train[:,5],X_train[:,6],X_train[:,7]], y_train,
+                    batch_size=256,epochs=50,shuffle=True,class_weight= dic_weights,callbacks=[checkpoint],
+                    validation_data = ([X_test[:,0],X_test[:,1],X_test[:,2],X_test[:,3],X_test[:,4],X_test[:,5],X_test[:,6],X_test[:,7]],y_test))
+
+    model.load_weights(checkpoint_filepath)
+    
+
+    print(model.evaluate([X_test[:,0],X_test[:,1],X_test[:,2],X_test[:,3],X_test[:,4],X_test[:,5],X_test[:,6],X_test[:,7]],y_test,batch_size=256))
+
+if FEATURE_NUMBERS==6:
+    # instantiate middle model
+    history = model.fit([X_train[:,0],X_train[:,1],X_train[:,2],X_train[:,3],X_train[:,4],X_train[:,5]], y_train,
+                    batch_size=256,epochs=50,shuffle=True,class_weight= dic_weights,callbacks=[checkpoint],
+                    validation_data = ([X_test[:,0],X_test[:,1],X_test[:,2],X_test[:,3],X_test[:,4],X_test[:,5]],y_test))
+
+    model.load_weights(checkpoint_filepath)
+    
+
+    print(model.evaluate([X_test[:,0],X_test[:,1],X_test[:,2],X_test[:,3],X_test[:,4],X_test[:,5]],y_test,batch_size=256))
+if FEATURE_NUMBERS==2:
+    history = model.fit([X_train[:,0],X_train[:,1]], y_train,
+                    batch_size=256,epochs=50,shuffle=True,class_weight= dic_weights,callbacks=[checkpoint],
+                    validation_data = ([X_test[:,0],X_test[:,1]],y_test))
+
+    model.load_weights(checkpoint_filepath)
+    
+
+    print(model.evaluate([X_test[:,0],X_test[:,1]],y_test,batch_size=256))
+
+model.save('models/IP_network_traffic_bit_'+str(BITWIDTH)+'_feature_'+str(FEATURE_NUMBERS)+'.h5')
+exit()
